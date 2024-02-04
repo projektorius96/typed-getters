1eac2e08f8dda726c2557931957505ef8de6d79c
LG <34185919+projektorius96@users.noreply.github.com>
2024-02-04 17:09:53 +0200

    init codebase

---
+++ src/index.mjs
@@ -0,0 +1,85 @@
+export default (function() {
+    const [__STRING__, __BOOL__, __INT__, __BIGINT__ , __SYMBOL__] = ['string', 'boolean', 'number', 'bigint', 'symbol'];
+    const TypedMap = new Map([
+        [__STRING__, Symbol('isString')],
+        [__BOOL__, Symbol('isBool')],
+        [__INT__, Symbol('isInt')],
+        [__BIGINT__, Symbol('isBint')],
+        [__SYMBOL__, Symbol('isSymbol')]
+    ]);
+    const TypedStruct = {
+        [TypedMap.get(__STRING__)]: function() {
+            if( typeof (this.valueOf() === __STRING__) ){
+                return this.valueOf();
+            }
+        },
+        [TypedMap.get(__BOOL__)]: function() {
+            if( typeof (this.valueOf() === __BOOL__) ){
+                return this.valueOf();
+            }
+        },
+        [TypedMap.get(__INT__)]: function() {
+            if( typeof (this.valueOf() === __INT__) ){
+                return this.valueOf();
+            }
+        },
+        [TypedMap.get(__BIGINT__)]: function() {
+            if( typeof (this.valueOf() === __BIGINT__) ){
+                return this.valueOf();
+            }
+        },
+        [TypedMap.get(__SYMBOL__)]: function() {
+            if( typeof (this.valueOf() === __SYMBOL__) ){
+                return this.valueOf();
+            }
+        },
+    };
+    Object.defineProperty(
+        String.prototype,
+        TypedMap.get(__STRING__).description,
+        {
+            get: TypedStruct[TypedMap.get(__STRING__)]
+        }
+    );
+    Object.defineProperty(
+        Boolean.prototype,
+        TypedMap.get(__BOOL__).description,
+        {
+            get: TypedStruct[TypedMap.get(__BOOL__)]
+        }
+    );
+    Object.defineProperty(
+        Number.prototype,
+        TypedMap.get(__INT__).description,
+        {
+            get: TypedStruct[TypedMap.get(__INT__)]
+        }
+    );
+    Object.defineProperty(
+        BigInt.prototype,
+        TypedMap.get(__BIGINT__).description,
+        {
+            get: TypedStruct[TypedMap.get(__BIGINT__)]
+        }
+    );
+    Object.defineProperty(
+        Symbol.prototype,
+        TypedMap.get(__SYMBOL__).description,
+        {
+            get: TypedStruct[TypedMap.get(__SYMBOL__)]
+        }
+    );
+}).call();
+
+/**
+ * 
+ * @param {Object} _struct - Object{key:value} pair(s);
+ * @param {Function} _interface - arbitrary implementation of 'typed-getters' matching against _struct (object);
+ * @param {Array} _args - list of arguments passed (if any);
+ * @returns {Object} instance of matched typed : **any type mismatch gracefully returns "undefined" value for each key of `_struct`.**
+ */
+export function new$(_struct, _interface, _args = []){
+    return (
+        Reflect.construct(_struct, _args, _interface/*  || _struct */)
+    )
+}
---
+++ test.mjs
@@ -0,0 +1,33 @@
+import { new$ } from "./src/index.mjs";
+
+// DEV_NOTE # decoupled constructor (struct)
+class Portfolio$struct {
+
+    constructor(age, name, isEmployed) {
+        this.age = age?.isInt,
+        this.name = name?.isString,
+        this.employed = isEmployed?.isBool
+    }
+
+}
+
+// DEV_NOTE # decoupled method(s) link to the struct
+class Portfolio$interface extends Portfolio$struct {
+    getProfile() {
+        console.log(
+            `
+            age=${this.age}
+            name=${this.name}
+            employed=${this.employed}
+            `
+        );
+    }
+}
+
+console.log(
+    new$(Portfolio$struct, Portfolio$interface, [2024, "John Doe", false])/* .getProfile() */ // # {age: 2024, name: 'John Doe', employed: false}
+    ,
+    "\n\n"
+    , 
+    new$(Portfolio$struct, Portfolio$interface, ["2024", "John Doe", "false"])/* .getProfile() */// # {age: undefined, name: 'John Doe', employed: undefined}
+)
